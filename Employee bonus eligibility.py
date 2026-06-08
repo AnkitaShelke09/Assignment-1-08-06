8119f202be4f20457eb4829b4ca13c5e2d36a400
AnkitaShelke09 <shelkeankita368@gmail.com>
2026-06-09 00:06:06 +0530

    Employee bonus eligibility program

---
+++ Employee bonus eligibility.py
@@ -0,0 +1,9 @@
+#Employee bonus eligibility
+
+name=input("Enter employee name:")
+work_done=float(input("Enter work done: "))
+if work_done>70:
+    print("You are eligible for bonus")
+
+else:
+    print("You are not eligible for bonus")
