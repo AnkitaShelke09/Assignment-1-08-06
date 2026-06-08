777ee69352fb94b2651b1d7da7d4cdfb3b2d09b2
AnkitaShelke09 <shelkeankita368@gmail.com>
2026-06-09 01:33:48 +0530

    Memory Address Comparison

---
+++ Memory Address Comparison.py
@@ -0,0 +1,12 @@
+#Memory Address Comparison
+
+num1 = [10000]
+num2 = num1
+
+print("Address of num1:", id(num1))
+print("Address of num2:", id(num2))
+
+if num1 is num2:
+    print("Same memory address")
+else:
+    print("Different memory addresses")
