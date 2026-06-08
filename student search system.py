bd22382bd7116dbae4471cd1c9894df7920977bc
AnkitaShelke09 <shelkeankita368@gmail.com>
2026-06-09 01:06:43 +0530

    Student search system program

---
+++ student search system.py
@@ -0,0 +1,10 @@
+# Student search system
+
+students = ["Ankita", "Aditi", "Nishi", "Anjali" , "Vaishu"]
+
+name = input("Enter student name: ")
+
+if name in students:
+    print("Student found")
+else:
+    print("Student not found")
