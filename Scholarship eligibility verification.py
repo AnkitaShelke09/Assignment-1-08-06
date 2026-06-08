54806f53b1d6b3383534e488d9a0cc8c25f11879
AnkitaShelke09 <shelkeankita368@gmail.com>
2026-06-09 00:56:44 +0530

    scholarship eligibility verification program

---
+++ Scholarship eligibility verification.py
@@ -0,0 +1,8 @@
+# scholarship eligibility verification
+
+name=input("Enter student name: ")
+perc=float(input("Enter percent of student: "))
+if perc>=80:
+    print("You are eligible for scholarship")
+else:
+    print("You are not eligible for scholarship")
