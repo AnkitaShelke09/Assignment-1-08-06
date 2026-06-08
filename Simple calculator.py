d11e3292c7e07e73bac72fe9afc3885c63c0252e
AnkitaShelke09 <shelkeankita368@gmail.com>
2026-06-08 21:22:58 +0530

    Simple calculator program

---
+++ Simple calculator.py
@@ -0,0 +1,11 @@
+     #Simple Calculator
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+
+     #Operations
+print("addition of a and b is",a+b)
+print("subtraction of a and b is",a-b)
+print("multiplication of a and b is",a*b)
+print("division of a and b is",a/b)
+print("modulo of a and b is",a%b)
+print("exponent of a and b is",a**b)
